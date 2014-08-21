c07aa3e37f7461ebc930054d8d7315215b8c7e22
Nicolas <nicolas@diffuzed.com>
2014-08-21 17:05:44 +0200

    Describe package with script.py file

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+setuptools.setup(
+    name='Navigator',
+    version='0.1',
+    description='Database schema navigator',
+    author='Nicolas Mussat',
+    author_email='nicolas@diffuzed.com',
+    url='https://github.com/nmussat/Navigator',
+    packages=['navigator'],
+    test_suite='tests'
+)
